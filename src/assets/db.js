882c85cd7e8922daa34f93abb7e98711d8e8fc5e
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-19 13:55:56 +0300

    simple update

--- src/assets/db.js
+++ src/assets/db.js
@@ -4,6 +4,7 @@ export const events = [
     description: "Use any relational or non-relational database.",
     date: "18.09.2024",
     organizer: "Artem",
+    id: 1,
   },
   {
     title: "React",
@@ -11,18 +12,21 @@ export const events = [
       "The back-end part can be done in NodeJS with or without any framework.",
     date: "19.09.2024",
     organizer: "Anna",
+    id: 2,
   },
   {
     title: "Node js",
     description: "with any preferred design style.",
     date: "18.09.2024",
     organizer: "Oleg",
+    id: 3,
   },
   {
     title: "HTML",
     description: "List of events can be pre-populated in the database.",
     date: "20.09.2024",
     organizer: "Igor",
+    id: 4,
   },
   {
     title: "CSS",
@@ -30,11 +34,13 @@ export const events = [
       " Event registration page: by clicking on “Register”, users should be redirected to the.",
     date: "21.09.2024",
     organizer: "Sergio",
+    id: 5,
   },
   {
     title: "Express js",
     description: "Use any relational or non-relational database.",
     date: "22.09.2024",
     organizer: "Olena",
+    id: 6,
   },
 ];
