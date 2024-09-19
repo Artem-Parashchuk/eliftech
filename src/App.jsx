5b9c4d45bbef01e1a3cc11ca9fcd0c27a2777992
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-19 13:56:44 +0300

    update syyles for App & index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import s from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
+import { Registration } from "./pages/Registration/Registration";
+import { AwesomeEvent } from "./pages/AwesomeEvent/AwesomeEvent";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
-      <div className={s.container}>
-        <Header/>
-        <Home />
-      </div>
+    <div className={s.container}>
+      <Header />
+      <main className={s.main}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/registration" element={<Registration />}/>
+        <Route path="/view" element={<AwesomeEvent/>}/>
+      </Routes>
+      </main>
+      <Footer/>
+    </div>
   );
 }
 
