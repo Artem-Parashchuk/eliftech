0e60069b49aa87dfe11402868ac5001e791a1925
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-22 19:48:41 +0300

    update props

--- src/components/AwesomeItem/AwesomeItem.jsx
+++ src/components/AwesomeItem/AwesomeItem.jsx
@@ -3,8 +3,8 @@ import s from './AwesomeItem.module.css'
 export const AwesomeItem = ({user}) => {
   return (<>
     <li className={s.item}>
-        <h3 className={s.title}>{user.name}</h3>
-        <p className={s.email}>{user.email}</p>
+        <h3 className={s.title}>{user.userName}</h3>
+        <p className={s.email}>{user.userEmail}</p>
     </li>
   </>
   )
