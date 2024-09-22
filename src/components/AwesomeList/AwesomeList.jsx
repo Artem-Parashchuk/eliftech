26513be6ab5a3d977a66167da1a11b8f8ae92c4d
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-22 21:12:24 +0300

    add filter to users

--- src/components/AwesomeList/AwesomeList.jsx
+++ src/components/AwesomeList/AwesomeList.jsx
@@ -1,48 +1,69 @@
 import { useDispatch, useSelector } from "react-redux";
-
 import {
+  selectFilteredUsers,
   selectUsers,
   selectUsersError,
   selectUsersLoading,
 } from "../../redux/users/selectors.js";
 import { fetchUsersThunk } from "../../redux/users/thunks.js";
-
 import { AwesomeItem } from "../AwesomeItem/AwesomeItem.jsx";
 import s from "./AwesomeList.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Loader } from "../Loader/Loader.jsx";
 
 export const AwesomeList = () => {
+  const [searchTerm, setSearchTerm] = useState(""); // Локальний стан для пошукового запиту
   const users = useSelector(selectUsers);
   const loading = useSelector(selectUsersLoading);
   const error = useSelector(selectUsersError);
-
+  const filters = useSelector(selectFilteredUsers)
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchUsersThunk());
   }, [dispatch]);
 
+  // Обробник зміни для текстового поля
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Фільтрація користувачів за ім'ям та імейлом
+  const filteredUsers = users?.data?.filter((user) => {
+    const userName = user.userName.toLowerCase();
+    const userEmail = user.userEmail.toLowerCase();
+    const searchValue = searchTerm.toLowerCase();
+    return (
+      userName.includes(searchValue) || userEmail.includes(searchValue)
+    );
+  });
 
   if (error) {
-   return <p className={s.error_message}>
-      Sorry, something went wrong. We are working on it.
-    </p>;
-  }
-
-  if (loading || !users || !users.data || users.data.length === 0) {
     return (
-      <>
-        <Loader />
-      </>
+      <p className={s.error_message}>
+        Sorry, something went wrong. We are working on it.
+      </p>
     );
   }
 
+  if (loading || !users || !users.data || users.data.length === 0) {
+    return <Loader />;
+  }
+
   return (
-    <ul className={s.list}>
-      {users.data.map((user) => {
-        return <AwesomeItem key={user._id} user={user} />;
-      })}
-    </ul>
+    <div>
+      <input
+        type="text"
+        placeholder="Search by name or email"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className={s.search_input} // Додай клас для стилізації
+      />
+      <ul className={s.list}>
+        {filteredUsers.map((user) => (
+          <AwesomeItem key={user._id} user={user} />
+        ))}
+      </ul>
+    </div>
   );
 };
--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -3,3 +3,5 @@
 export const selectUsers = state => state.users.users;
 export const selectUsersLoading = state => state.users.loading;
 export const selectUsersError = state => state.users.error;
+
+export const selectFilteredUsers = state => state.users.filters
